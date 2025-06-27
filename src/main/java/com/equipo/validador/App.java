5667d2f02f5619d03976bc85435860ba6be1a17c
cras-1296 <caraya.salazar@gmail.com>
2025-06-27 00:02:15 -0400

    Cuarto commit: Se implementa logger en App.java

--- src/main/java/com/equipo/validador/App.java
+++ src/main/java/com/equipo/validador/App.java
@@ -1,9 +1,17 @@
 package com.equipo.validador;
-		public class App {
- 		public static void main(String[] args) {
- 			String usuario = System.getenv("APP_USER");
- 			if ("admin".equals(usuario)) {
- 				System.out.println("¡Bienvenido administrador!");
- 			}
- 		}
-	}
+
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+public class App {
+    private static final Logger logger = Logger.getLogger(App.class.getName());
+    
+    public static void main(String[] args) {
+        String usuario = System.getenv("APP_USER");
+        if ("admin".equals(usuario)) {
+            logger.info("¡Bienvenido administrador!");
+        } else {
+            logger.log(Level.WARNING, "Usuario no autorizado o variable de entorno no definida");
+        }
+    }
+}
